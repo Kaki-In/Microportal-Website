334744ce28bdba2baa643a61640fe36ab3498e30
Kaki In <kaki@mifamofi.net>
2023-09-01 16:51:51 +0200

    Added privacy informations

--- scripts/ui/main/ServerSettingsPage.js
+++ scripts/ui/main/ServerSettingsPage.js
@@ -26,6 +26,9 @@ function createServerSettings(platform) {
 
     let div = maindiv.appendChild(document.createElement("div"));
 
+    div.appendChild(document.createElement("h1")).textContent = "Informations sur les données personnelles";
+    div.appendChild(document.createElement("p")).textContent = "Microportal nécessite une connexion non sécurisée (http://) pour fonctionner. Cette connexion sert uniquement à récupérer les informations de l'application en ligne. Toutes informations échangées entre votre appareil et votre serveur Microportal personnel seront gardées intactes à l'intérieur de votre réseau. Aucune information personnelle de quelque sorte n'est envoyé en ligne.";
+
     div.appendChild(document.createElement("h1")).textContent = "Qu'est-ce que Microportal?";
     div.appendChild(document.createElement("p")).textContent = "Microportal est une platforme domotique open source qui vous permet de gérer vos appareils de manière sécurisée, privée et rapide. ";
 
@@ -35,9 +38,6 @@ function createServerSettings(platform) {
 
     div.appendChild(document.createElement("h1")).textContent = "Installation des clients";
     div.appendChild(document.createElement("p")).innerHTML = `Le code source officiel des codes sources des robots pour Microportal se trouve <a href="https://github.com/Kaki-In/Microportal-Robot" target="_blank"> sur Github </a>`;
-
-    div.appendChild(document.createElement("p"));
-    div.appendChild(document.createElement("p")).textContent = "Microportal nécessite une connexion non sécurisée (http://) pour fonctionner. Cette connexion sert uniquement à récupérer les informations de l'application en ligne. Toutes informations échangées entre votre appareil et le serveur Microportal seront gardées intactes à l'intérieur de votre réseau. Aucune information personnelle de quelque sorte n'est envoyé en ligne.";
 
     return [ maindiv, form ];
 }
